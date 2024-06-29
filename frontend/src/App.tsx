5ee54b64026e18d63e4123b19218feec29896442
Your Name <shuvongkarroy13@gmail.com>
2024-06-29 23:48:21 +0600

    JWT authentication and tested with Playwright

---
+++ frontend/src/App.tsx
@@ -0,0 +1,37 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Layout from './layouts/Layout'
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+
+function App() {
+ 
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={
+        <Layout>
+          <p>Home</p>
+        </Layout>} />
+
+        <Route path="/register" element={
+        <Layout>
+          <Register/>
+        </Layout>} />
+
+        <Route path="/sign-in" element={
+        <Layout>
+          <SignIn/>
+        </Layout>} />
+        
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
